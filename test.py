6ed3be41f7ba355cfe8e26ac40db5c31def01454
Špela Novak <spela.novak2@student.fmf.uni-lj.si>
2019-04-24 15:09:45 +0200

    still trying to get a merge conflict

--- test.py
+++ test.py
@@ -8,12 +8,19 @@
 #  seznam = []
 # def prastevila(stevila):
 #     if je_prastevilo(n)
-def je_prastevilo(n):
+def nskljfnv(n):
     for i in range(2, n):
         if n % i == 0:
-            return False
-    return True
+        
 
 for x in range(2, 201):
     if je_prastevilo(x):
         print(y)
+
+
+
+bjdksbnc
+
+
+vmclksd
+sod
